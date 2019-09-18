ae33b3b87799d49bf3711fe0db891194fa0e6faf
zolizoli <zoltan.varju@gmail.com>
2019-09-18 09:24:48 +0200

    add chapter 3 code - no documentation and type hints as it'd be an overkill

---
+++ src/recursion.py
@@ -0,0 +1,28 @@
+def countdown(i):
+    print(i)
+    if i <= 0:
+        return
+    else:
+        countdown(i-1)
+
+
+def greet(name):
+    print("hello, " + name + "!")
+    greet2(name)
+    print("getting ready to say bye...")
+    bye()
+
+
+def greet2(name):
+    print("how are you, " + name + "?")
+
+
+def bye():
+    print("ok bye!")
+
+
+def fact(x):
+    if x == 1:
+        return 1
+    else:
+        return x * fact(x-1)
